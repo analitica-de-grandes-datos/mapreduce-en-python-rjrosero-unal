2a0d216bb2d1874c27396d5db51ac04085a95bb0
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-07 21:40:41 -0500

    Pregunta1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+        fields = line.strip().split(',',3)
+        credit_history =  fields[2]
+
+        sys.stdout.write("{}\t1\n".format(credit_history))
