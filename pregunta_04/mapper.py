ec14389d3a3c68b88fe2fdf70b763e32ce671b42
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 18:41:27 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
 
     for line in sys.stdin:
-        fields = line.strip().split(',',1)
+        fields = line.strip().split('   ',2)
         letra = fields[0]
 
-        sys.stdout.write("{}\t{}\n".format(letra))
+        sys.stdout.write("{}\t1\n".format(letra))
