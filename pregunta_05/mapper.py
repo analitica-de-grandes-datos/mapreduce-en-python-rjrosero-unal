a2616c71e1543b16be43b0552b33dfcae39e3fa8
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 18:51:35 -0500

    Pregunta5

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('   ',3)
+        fecha = fields[1]
+        fechaSeparada = fecha.split('-')
+        mes=fechaSeparada[1]
+
+        sys.stdout.write("{}\t1\n".format(mes))
