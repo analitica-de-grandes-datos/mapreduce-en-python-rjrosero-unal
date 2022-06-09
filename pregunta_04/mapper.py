62cdd45381c5029aba48bc8df6372ed78cd793e0
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 18:35:35 -0500

    Pregunta4

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split(',',1)
+        letra = fields[0]
+
+        sys.stdout.write("{}\t{}\n".format(letra))
