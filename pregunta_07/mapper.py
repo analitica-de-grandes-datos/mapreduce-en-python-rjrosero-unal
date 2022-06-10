d6b8420e3933a0ff15d58f9242db95114ee586ae
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 20:08:00 -0500

    Pregunta7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('   ',3)
+        letra=fields[0]
+        fecha=fields[1]
+        numero=fields[2]
+        
+        sys.stdout.write("{}\t{}\t{}\n".format(letra,fecha,numero))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    listaTuplas = []
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, fecha, numero = line.split("\t")
+        numero = int(numero)
+        tupla = key, fecha, numero
+        listaTuplas.append(tupla)
+        
+    #ordernar el diccionario
+    tuplaOrdenada = sorted(listaTuplas, key=lambda x: (x[0],x[2]))
+
+    #imprimir el diccionario
+    for tupla in tuplaOrdenada:
+        sys.stdout.write("{}\t{}\t{}\n".format(tupla[0], tupla[1], tupla[2]))
+
+    
+        
