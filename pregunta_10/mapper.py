bf235739a2936e1c103b971d5f29a1946dc5996b
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 21:09:31 -0500

    Pregunta10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('\t',2)
+        numero=fields[0]
+        letras=fields[1]
+                
+        sys.stdout.write("{}\t{}\n".format(numero,letras))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,31 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    diccionarioLetras = {}
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = val.split(",")
+
+        #por cada letra de val obtener cada uno de los key asociados
+        for i in val:
+            diccionarioLetras[i].append(key)
+
+        sortedDict = sorted(diccionarioLetras.items(), key=lambda x: x[0])
+
+        for k, v in sortedDict:
+            res = sorted(v, key = int)
+            v = ",".join(map(str, res))
+
+            sys.stdout.write("{}\t{}\n".format(k, v))
