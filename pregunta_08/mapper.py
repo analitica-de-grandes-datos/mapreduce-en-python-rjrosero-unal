5d19c519b0cce26a9cd60b0386afc4fbb0436a17
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 20:20:24 -0500

    Pregunta8

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('   ',3)
+        letra=fields[0]
+        numero=fields[2]
+        
+        sys.stdout.write("{}\t{}\n".format(letra,numero))
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,46 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from audioop import avg
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    listaElementos = []
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+        
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            listaElementos.append(val)
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                suma = sum(listaElementos)
+                prom = suma / len(listaElementos)
+                listaElementos.clear()                
+
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, prom))
+
+            curkey = key
+            listaElementos.append(val)               
+
+    suma = sum(listaElementos)
+    prom = suma / len(listaElementos)
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, prom))
