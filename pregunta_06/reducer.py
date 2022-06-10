d286606bf0c2d9d2aff356cb5b2198588516affc
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 19:40:33 -0500

    Pregunta6

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,45 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    listaElementos = []
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+        
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            listaElementos.append(val)
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                maximo = max(listaElementos)
+                minimo = min(listaElementos)
+                listaElementos.clear()                
+
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, minimo))
+
+            curkey = key
+            listaElementos.append(val)               
+
+    maximo = max(listaElementos)
+    minimo = min(listaElementos)
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, minimo))
