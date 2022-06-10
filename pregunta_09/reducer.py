c93238b1f3724cafb876c45e0080eb005ccf8459
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-06-09 20:44:05 -0500

    Pregunta9

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    listaTuplas = []
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, fecha, numero = line.split("\t")
+        numero = int(numero)
+        tupla = key, fecha, numero
+        listaTuplas.append(tupla)
+        
+    #ordernar el diccionario
+    tuplaOrdenada = sorted(listaTuplas, key=lambda x: x[2])
+
+    #imprimir diccionario
+    for tupla in tuplaOrdenada[0:6]:
+        sys.stdout.write("{}\t{}\t{}\n".format(tupla[0], tupla[1], tupla[2]))
